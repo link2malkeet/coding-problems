0f7ab4a949a5ff883394098a73c0fd53571b101e
mel <malkeet.singh@lendi.com.au>
2023-04-11 23:21:48 +1000

    atoi

---
+++ atoi.ts
@@ -0,0 +1,39 @@
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// Read in and ignore any leading whitespace.
+// Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+// Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+// Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+// If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+// Return the integer as the final result.
+function myAtoi(s: string) {
+  let i = 0;
+  let sign = 1;
+  let result = 0;
+  while (s[i] === " ") {
+    i++;
+  }
+  if (s[i] === "+" || s[i] === "-") {
+    sign = s[i] === "-" ? -1 : 1;
+    i++;
+  }
+
+  while (i <= s.length && /\d/.test(s[i])) {
+    result = result * 10 + Number(s[i]);
+    i++;
+  }
+
+  result = sign * result;
+
+  const MAX_INTEGER_RANGE = 2 ** 31 - 1;
+  const MIN_INTEGER_RANGE = -(2 ** 31);
+  if (result > MAX_INTEGER_RANGE) {
+    return MAX_INTEGER_RANGE;
+  } else if (result < MIN_INTEGER_RANGE) {
+    return MIN_INTEGER_RANGE;
+  } else {
+    return result;
+  }
+}
