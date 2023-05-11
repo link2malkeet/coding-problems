e3b4e31f2cd7b641d077d9413321260dac36a15c
mel <malkeet.singh@lendi.com.au>
2023-05-11 22:13:27 +1000

    logestPrefix, IntegertoRoman

--- longestCommonPrefix.ts
+++ longestCommonPrefix.ts
@@ -2,8 +2,6 @@
 
 // If there is no common prefix, return an empty string "".
 
- 
-
 // Example 1:
 
 // Input: strs = ["flower","flow","flight"]
@@ -15,19 +13,19 @@
 // Explanation: There is no common prefix among the input strings.
 
 function longestCommonPrefix(strs: string[]): string {
-    let prefix = strs[0];
+  if (strs.length === 0) {
+    return "";
+  }
 
-    for (let i = 1; i < strs.length; i++) {
-        while(strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-            if(prefix.length ===0) {
-                return ""
-            }
-        }
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
     }
+  }
 
-    return prefix;
-};
-
-console.log(longestCommonPrefix(["flower","flow","flight"]))
-console.log(longestCommonPrefix(["dog","racecar","car"]));
+  return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "hjhk"]));
