80c85c6cf89661957f3d81dd5380d3ca83c1e6d2
mel <malkeet.singh@lendi.com.au>
2023-06-04 23:48:26 +1000

    async editorial

---
+++ js/8.ts
@@ -0,0 +1,8 @@
+async function sleep(millis: number): Promise<void> {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(), millis);
+  });
+}
+
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
