034d91d498af3f336687af0d5e0824c08f674a8b
mel <malkeet.singh@lendi.com.au>
2023-07-04 00:40:41 +1000

    countQuadruplets.ts

--- arrays/countQuadruplets.ts
+++ arrays/countQuadruplets.ts
@@ -11,28 +11,17 @@
 
 (() => {
   function countQuadruplets(nums: number[]): number {
-    const sumCounts: Map<number, number> = new Map();
     const n = nums.length;
     let count = 0;
 
-    // Count the occurrences of the sums of pairs of elements
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = i + 1; j < n; j++) {
-        const sum = nums[i] + nums[j];
-        if (sumCounts.has(sum)) {
-          sumCounts.set(sum, sumCounts.get(sum)! + 1);
-        } else {
-          sumCounts.set(sum, 1);
-        }
-      }
-    }
-
-    // Check for quadruplets that satisfy the requirement
-    for (let c = 2; c < n - 1; c++) {
-      for (let d = c + 1; d < n; d++) {
-        const dSum = nums[c] + nums[d];
-        if (sumCounts.has(dSum)) {
-          count += sumCounts.get(dSum)!;
+    for (let a = 0; a < n; a++) {
+      for (let b = a + 1; b < n; b++) {
+        for (let c = b + 1; c < n; c++) {
+          for (let d = c + 1; d < n; d++) {
+            if (nums[a] + nums[b] + nums[c] === nums[d]) {
+              count++;
+            }
+          }
         }
       }
     }
