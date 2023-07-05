f08e8a696399878730c13ad1c4ddebe19a20c2d9
mel <malkeet.singh@lendi.com.au>
2023-07-06 01:06:38 +1000

    subarray sum

--- arrays/subarraySum.ts
+++ arrays/subarraySum.ts
@@ -12,7 +12,25 @@
 // Output: 2
 
 (() => {
+  // without space
   function subarraySum(nums: number[], k: number): number {
+    let count = 0;
+    for (let start = 0; start < nums.length; start++) {
+      let sum = 0;
+      for (let end = start; end < nums.length; end++) {
+        sum += nums[end];
+        if (sum === k) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  console.log("subarraySum:", subarraySum([3, 4, 7, 2, -3, 1, 4, 2], 7));
+  // brute force
+  function subarraySum_2(nums: number[], k: number): number {
     let count = 0;
     for (let start = 0; start < nums.length; start++) {
       for (let end = start + 1; end <= nums.length; end++) {
